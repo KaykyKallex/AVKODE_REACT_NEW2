32592dc0df4202e0349ef54f99c2ebf1a83cb20b
KaykyKallex <desouzakayky05@gmail.com>
2023-11-21 21:33:17 -0300

    adicionando efeitos no site

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,10 +4,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import { motion, useScroll, useSpring } from "framer-motion";
+import myImage from './theus.jpeg';
+import myImage1 from './edu.jpeg';
+import Slide from 'react-reveal/Slide';
 
 function Home() {
   const { scrollYProgress } = useScroll();
@@ -60,14 +60,20 @@ function Home() {
           </Carousel>
         </div>
       </section>
+
+      
       <section id='SobreNos'>
         <div className='sobre-page'>
+          <Slide bottom>
           <h1 id='sbpH1'>Sobre Nós</h1>
           <Image src="https://c4.wallpaperflare.com/wallpaper/59/531/787/5bd0338b20c50-wallpaper-preview.jpg" fluid />;
           <p id='sbpP'>A AVKODE é uma empresa especializada em desenvolvimento de software, oferecendo soluções confiáveis e personalizadas. Nossa principal missão é impulsionar o sucesso de nossos clientes, criando produtos de alta qualidade em um ambiente tecnologicamente desafiador e em constante mudança. Acreditamos no poder da tecnologia para transformar negócios e estamos comprometidos em fornecer as melhores soluções para atender às necessidades de nossos clientes.</p>
+        </Slide>
         </div>
       </section>
+      
 
+      <Slide bottom>
       <section id='Contato'>
         <div className='contato-page'>
           <Form className='cnt-page'>
@@ -86,6 +92,7 @@ function Home() {
           </Form>
         </div>
       </section>
+      </Slide>
 
       <div className='divisor'></div>
 
@@ -94,29 +101,33 @@ function Home() {
         <div className='portifolio-page'>
           <h1 id='portifolio-h1'>Nossos Trabalhos</h1>
           <div className='pPOrtifolio-page'>
+          <Slide bottom>
             <Image className='img-port' src="https://c4.wallpaperflare.com/wallpaper/59/531/787/5bd0338b20c50-wallpaper-preview.jpg" fluid />;
             <p id='pPort'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <Image className='img-port' src="https://c4.wallpaperflare.com/wallpaper/59/531/787/5bd0338b20c50-wallpaper-preview.jpg" fluid />;
             <p id='pPort'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Slide>
           </div>
         </div>
       </section>
 
       <section id='Agradecimentos'>
         <div className='Agradecimentos-page'>
+        <Slide left>
           <h1 id='Agradecimentos-h1'>Equipe AVKODE</h1>
           <div className='Agradecimentos-img'>
-            <Image className='img-agrd' src="https://media.licdn.com/dms/image/D4D35AQHOARFCULbr3g/profile-framedphoto-shrink_200_200/0/1692196856003?e=1700596800&v=beta&t=NCp9YcenmTMEnWdCOsv4iMosa6zfP2idtM9DnEmB_qs" fluid />;
+            <Image className='img-agrd' src="https://media.licdn.com/dms/image/D4D03AQH7Xo4n4dUVeQ/profile-displayphoto-shrink_200_200/0/1681913003048?e=1706140800&v=beta&t=BzzHvKZNqR-sUzKKytHvFKkgyrcQPdmWwtRZCq90NuI" fluid />;
             <p id='agrP'>Carlos Eduardo</p>
-            <Image className='img-agrd' src="https://media.licdn.com/dms/image/D4D03AQH7NRw8L2rjCg/profile-displayphoto-shrink_200_200/0/1682715618568?e=1705536000&v=beta&t=W0giodxPN751ItsyAx33DEm0D0pQOBJ5eKW8rKKseLM" fluid />;
+            <Image className='img-agrd' src={myImage1} fluid />;
             <p id='agrP'>Eduardo Pereira</p>
             <Image className='img-agrd' src="https://media.licdn.com/dms/image/D4E03AQHnDcnP-jviDA/profile-displayphoto-shrink_200_200/0/1692973791724?e=1705536000&v=beta&t=1Bo_GM9v3WpBzw3cbCoqEEzLGTYrJJHg0oQ3U2wG_K8" fluid />;
             <p id='agrP'>Kayky de Souza</p>
-            <Image className='img-agrd' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" fluid />;
+            <Image className='img-agrd' src={myImage} fluid />;
             <p id='agrP'>Matheus Ferreira</p>
             <Image className='img-agrd' src="https://media.licdn.com/dms/image/D4D03AQFuB0sXF2h5Ag/profile-displayphoto-shrink_200_200/0/1697399435451?e=1705536000&v=beta&t=0Yh1e8k5pDR381Bg_BwxnBEF77C-vzjUp8CgvUBo8fU" fluid />;
-            <p id='agrP'>Victor Massao</p>
+            <p id='agrP'>Victor Massao</p>  
           </div>
+          </Slide>
         </div>
       </section>
     </>
